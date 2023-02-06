2751933672768b408d3f350141b3d2cd8dc04861
aarti283 <aartibhawsar283@gmail.com>
2023-02-06 12:10:35 +0530

    "java assignment"

---
+++ src/main/java/com/example/practice/controller/StudentController.java
@@ -0,0 +1,48 @@
+package com.example.practice.controller;
+
+import java.util.Optional;
+import com.example.practice.model.Student;
+import com.example.practice.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/allstudents")
+    public List<Student> getAllStudents() {
+        return this.studentService.getStudents();
+    }
+
+    @PostMapping("/createstudent")
+    public String createStudent(@RequestBody Student student) {
+        return this.studentService.createStudent(student);
+    }
+    // delete
+
+    @DeleteMapping("/deletestudent")
+    public String deleteStudent(@RequestBody Student student) {
+        return this.studentService.deleteStudent(student);
+    }
+
+    // update
+    @PutMapping("/updatestudent")
+    public boolean updateStudent(@RequestBody Student student) {
+        return this.studentService.updateStudent(student);
+    }
+
+    // getbyid
+    @GetMapping("/getstudentbyid")
+    public Optional<Student> getStudentById(@RequestBody Student student) {
+        return this.studentService.getStudentById(student);
+    }
+}
---
+++ src/main/java/com/example/practice/service/StudentService.java
@@ -0,0 +1,54 @@
+package com.example.practice.service;
+
+import java.util.Optional;
+import com.example.practice.model.Student;
+import com.example.practice.repository.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepo studentRepo;
+
+    // Create New student details
+
+    public String createStudent(Student student) {
+        if (this.studentRepo.existsById(student.getId())) {
+            return "Student already exists";
+        } else {
+            this.studentRepo.save(student);
+            return "Student details are saved.";
+        }
+    }
+
+    // get all student details
+
+    public List<Student> getStudents() {
+        return this.studentRepo.findAll();
+
+    }
+
+    // delete student
+    public String deleteStudent(Student student) {
+        this.studentRepo.deleteById(student.getId());
+        return "Student deleted";
+
+    }
+
+    // update student
+    public boolean updateStudent(Student student) {
+        return true;
+    }
+
+    // get student by id
+
+    public Optional<Student> getStudentById(Student student) {
+        return this.studentRepo.findById(student.getId());
+
+    }
+
+}
